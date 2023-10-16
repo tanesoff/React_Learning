c4a18f3eb54f865a9a441a894e60d12176e7cc92
Viktor Tanevski <tanevski.viktor@outlook.com>
2023-10-16 21:48:59 +0200

    Done with star rating component

--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -9,7 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating color="red" maxRating={10} size={24} defaultRating={3} />
   </React.StrictMode>
 );
