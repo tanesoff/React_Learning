80807ad05c5aa464cfb66e9845dc86feefee6241
Viktor Tanevski <tanevski.viktor@outlook.com>
2023-10-18 21:00:53 +0200

    Added show and hide text to the spans in the main app

--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
+import App from "./ch1";
 
 import StarRating from "./StarRateing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating color="red" maxRating={10} size={24} defaultRating={3} />
+    <StarRating color="red" maxRating={10} size={24} defaultRating={3} /> */}
   </React.StrictMode>
 );
